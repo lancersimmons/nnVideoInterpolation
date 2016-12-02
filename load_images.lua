8f857ff0e0431d267f7bacbcce125c730c108219
Lance Simmons <lancersimmons@gmail.com>
2016-12-02 01:33:13 -0500

    fixed size param

--- load_images.lua
+++ load_images.lua
@@ -5,24 +5,26 @@ require 'image'
 local module = {}
 
 function module.load(path, n)
-    h = 128
-    w = h
+    
+    -- SIZE PARAMETERS
+    h = 96
+    w = 160
 
     images = torch.Tensor(n, 3, h, w)
 
-    print("Start:  Loading.")
+    -- print("Start:  Loading.")
 
     for i=1,n do
---        print('loading', i)
+        -- print('loading', i)
         I = image.load(string.format('%s/frame%05d.jpg', path, i-1))
---        print('done with image load')
+        -- print('done with image load')
         images[i] = I
---        print('done loading')
+        -- print('done loading')
     end
 
---    print('after for loop')
+    -- print('after for loop')
 
-    print("Finish: Loading.")
+    -- print("Finish: Loading.")
 
     return images
 end
