617bc4420df59e76f3ee144bfb5c1b96f82eb87f
Lance Simmons <lancersimmons@gmail.com>
2016-12-03 22:21:57 -0500

    nn doing prediction (poorly)

--- cnn.lua
+++ cnn.lua
@@ -16,7 +16,8 @@ numberOfInputFramesToUse = 300
 frameGroupSize = 3
 
 -- ARCHITECTURAL PARAMETERS
-batch_size = 100
+trainingCycles = 100
+batch_size = 1
 frame_width = 160
 frame_height = 96
 n_channels = 3
@@ -24,6 +25,7 @@ n_channels = 3
 
 
 -- CODE START:
+print("Start script...")
 
 -- set default tensor type to 32-bit float
 torch.setdefaulttensortype('torch.FloatTensor')
@@ -98,35 +100,21 @@ end
 
 
 
--- these tensors will hold the outer and inner frames of the triplets, respectively
-input_frames = torch.Tensor(batch_size, 2, n_channels, frame_height, frame_width)
-output_frames_middle = torch.Tensor(batch_size, n_channels, frame_height, frame_width)
+-- -- FIXME: Why is this causing the loss to be worse?
+-- -- Scale RGB from [0..1] to [-1..1] to satisfy Tanh
+-- trainingFrames = trainingFrames * 2
+-- trainingFrames = trainingFrames - 1
+
+-- testingFrames = testingFrames * 2
+-- testingFrames = testingFrames - 1
 
 
--- FIXME: Ensure generalization of this code for more input sizes / parameters\
--- This will move necessary frames into the input_frames, output_frames_middle tensors
-for frameNumber=1, batch_size, 1
-do
-	-- print("Frame number in batch: ", frameNumber) -- 1 to 100 inclusive
-	
-	input_frames[0+frameNumber][1] = trainingFrames[0+frameNumber]
-	input_frames[0+frameNumber][2] = trainingFrames[0+frameNumber+2]
-
-	output_frames_middle[0+frameNumber] = trainingFrames[0+frameNumber+1]
-
-
-end
-
--- -- FIXME: Remove SANITY CHECK
--- predictedImage = torch.Tensor(3,128,128)
--- predictedImage = input_frames[50][2]
-
--- filename = "test.jpg"
--- image.save(filename, predictedImage)
 
 
 -- FIXME: Fix the Tanh/relu scaling issue
 ------------ Model ------------------------------------------------
+
+criterion = nn.MSECriterion()
 
 s1 = nn.Sequential()
 s2 = nn.Sequential()
@@ -139,33 +127,33 @@ s8 = nn.Sequential()
 s9 = nn.Sequential()
 s10 = nn.Sequential()
 model = nn.Sequential()
-c1 = nn.Parallel(2, 1)
-c2 = nn.DepthConcat(1)
-c3 = nn.DepthConcat(1)
-c4 = nn.DepthConcat(1)
+c1 = nn.Parallel(2, 2)
+c2 = nn.DepthConcat(2)
+c3 = nn.DepthConcat(2)
+c4 = nn.DepthConcat(2)
 
 s1:add(nn.SpatialConvolution(3, 96, 3, 3, 1, 1, 1, 1)) ----- frame 1 conv block 1
-s1:add(nn.ReLU())
+s1:add(nn.Tanh())
 s1:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1))
-s1:add(nn.ReLU())
+s1:add(nn.Tanh())
 s1:add(nn.SpatialMaxPooling(2, 2, 2, 2))		--- ouput_size: 96 x fr_w/2 x fr_h/2
 
 s1:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1)) ------ frame 1 conv block 2
-s1:add(nn.ReLU())
+s1:add(nn.Tanh())
 s1:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1))
-s1:add(nn.ReLU())	
+s1:add(nn.Tanh())	
 s1:add(nn.SpatialMaxPooling(2, 2, 2, 2))		--- ouput_size: 96 x fr_w/4 x fr_h/4
 
 s2:add(nn.SpatialConvolution(3, 96, 3, 3, 1, 1, 1, 1)) ----- frame 3 conv block 1
-s2:add(nn.ReLU())
+s2:add(nn.Tanh())
 s2:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1))
-s2:add(nn.ReLU())
+s2:add(nn.Tanh())
 s2:add(nn.SpatialMaxPooling(2, 2, 2, 2))		--- ouput_size: 96 x fr_w/2 x fr_h/2
 
 s2:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1)) ----- frame 3 conv block 2
-s2:add(nn.ReLU())
+s2:add(nn.Tanh())
 s2:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1))
-s2:add(nn.ReLU())
+s2:add(nn.Tanh())
 s2:add(nn.SpatialMaxPooling(2, 2, 2, 2))		--- ouput_size: 96 x fr_w/4 x fr_h/4
 
 c1:add(s1)
@@ -174,82 +162,182 @@ c1:add(s2)											--- ouput_size: (96+96) x fr_w/4 x fr_h/4
 
 s3:add(c1)								--------------------- Conv block 3
 s3:add(nn.SpatialConvolution(192, 128, 3, 3, 1, 1, 1, 1)) 
-s3:add(nn.ReLU())
+s3:add(nn.Tanh())
 s3:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1))
-s3:add(nn.ReLU())
+s3:add(nn.Tanh())
 s3:add(nn.SpatialMaxPooling(2, 2, 2, 2))		--- ouput_size: 128 x fr_w/8 x fr_h/8
 
 
 s4:add(s3)								--------------------- Conv block 4
 s4:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1)) 
-s4:add(nn.ReLU())
+s4:add(nn.Tanh())
 s4:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1))
-s4:add(nn.ReLU())
+s4:add(nn.Tanh())
 s4:add(nn.SpatialMaxPooling(2, 2, 2, 2))		--- ouput_size: 128 x fr_w/16 x fr_h/16
 
 
 s5:add(s4)								--------------------- Conv block 5
 s5:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1)) 
-s5:add(nn.ReLU())
+s5:add(nn.Tanh())
 s5:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1))
-s5:add(nn.ReLU())
+s5:add(nn.Tanh())
 s5:add(nn.SpatialMaxPooling(2, 2, 2, 2))		--- ouput_size: 128 x fr_w/32 x fr_h/32
 
 
 s6:add(s5)								--------------------- DeConv block 1
 s6:add(nn.SpatialFullConvolution(128, 128, 4, 4, 2, 2, 1, 1))
-s6:add(nn.ReLU())
+s6:add(nn.Tanh())
 s6:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1)) 
-s6:add(nn.ReLU())									--- ouput_size: 128 x fr_w/16 x fr_h/16
+s6:add(nn.Tanh())									--- ouput_size: 128 x fr_w/16 x fr_h/16
 
 c2:add(s4)
 c2:add(s6)											--- ouput_size: (128+128) x fr_w/16 x fr_h/16
 
 s7:add(c2)								--------------------- DeConv block 2
 s7:add(nn.SpatialFullConvolution(256, 128, 4, 4, 2, 2, 1, 1))
-s7:add(nn.ReLU())
+s7:add(nn.Tanh())
 s7:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1)) 
-s7:add(nn.ReLU())									--- ouput_size: 128 x fr_w/8 x fr_h/8
+s7:add(nn.Tanh())									--- ouput_size: 128 x fr_w/8 x fr_h/8
 
 c3:add(s3)
 c3:add(s7)											--- ouput_size: (128+128) x fr_w/16 x fr_h/16
 
 s8:add(c3)								--------------------- DeConv block 3
 s8:add(nn.SpatialFullConvolution(256, 128, 4, 4, 2, 2, 1, 1))
-s8:add(nn.ReLU())
+s8:add(nn.Tanh())
 s8:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1)) 
-s8:add(nn.ReLU())									--- ouput_size: 128 x fr_w/4 x fr_h/4
+s8:add(nn.Tanh())									--- ouput_size: 128 x fr_w/4 x fr_h/4
 
 c4:add(c1)
 c4:add(s8)											--- ouput_size: (192+128) x fr_w/16 x fr_h/16
 
 s9:add(c4)								--------------------- DeConv block 4
 s9:add(nn.SpatialFullConvolution(320, 96, 4, 4, 2, 2, 1, 1))
-s9:add(nn.ReLU())
+s9:add(nn.Tanh())
 s9:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1)) 
-s9:add(nn.ReLU())									--- ouput_size: 96 x fr_w/2 x fr_h/2
+s9:add(nn.Tanh())									--- ouput_size: 96 x fr_w/2 x fr_h/2
 
 s10:add(s9)								--------------------- DeConv block 5
 s10:add(nn.SpatialFullConvolution(96, 96, 4, 4, 2, 2, 1, 1))
-s10:add(nn.ReLU())
+s10:add(nn.Tanh())
 s10:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1)) 
-s10:add(nn.ReLU())									--- ouput_size: 96 x fr_w/1 x fr_h/1
+s10:add(nn.Tanh())									--- ouput_size: 96 x fr_w/1 x fr_h/1
 
 model:add(s10)							--------------------- Output Layer
 model:add(nn.SpatialConvolution(96, 3, 3, 3, 1, 1, 1, 1))
-model:add(nn.ReLU())									--- ouput_size: 3 x fr_w/1 x fr_h/1
+model:add(nn.Tanh())									--- ouput_size: 3 x fr_w/1 x fr_h/1
 -------------------------------------------------------------------
 
--- FIXME: ENABLE THIS
+-- rand_inputs = torch.randn(batch_size, 2, n_channels, frame_height, frame_width)
+-- rand_inputs1 = torch.randn(n_channels, frame_height, frame_width)
+
+-- -- these tensors will hold the outer and inner frames of the triplets, respectively
+-- input_frames = torch.Tensor(batch_size, 2, n_channels, frame_height, frame_width)
+-- output_frames_middle = torch.Tensor(batch_size, n_channels, frame_height, frame_width)
+
+-- -- FIXME: Ensure generalization of this code for more input sizes / parameters
+-- -- This will move necessary frames into the input_frames, output_frames_middle tensors
+-- for frameNumber=1, batch_size, 1
+-- do
+-- 	-- print("Frame number in batch: ", frameNumber) -- 1 to 100 inclusive
+	
+-- 	input_frames[0+frameNumber][1] = trainingFrames[0+frameNumber]
+-- 	input_frames[0+frameNumber][2] = trainingFrames[0+frameNumber+2]
+
+-- 	output_frames_middle[0+frameNumber] = trainingFrames[0+frameNumber+1]
+
+-- end
 
 
-inputs = torch.randn(batch_size, 2, n_channels, frame_height, frame_width)
-inputs1 = torch.randn(n_channels, frame_height, frame_width)
-
--- FIXME: Why does this cause me to run out of ram?
--- I don't feel like it should...
 print("Forwarding...")
-aa = model:forward(inputs)
+-- forward random test inputs
+-- aa = model:forward(rand_inputs)
+-- forward real inputs
 -- aa = model:forward(input_frames)
-print(aa)
+-- print(aa)
+
+
+
+-- TRAINING
+
+
+-- these tensors will hold the outer and inner frames of the triplets, respectively
+input_frames = torch.Tensor(batch_size, 2, n_channels, frame_height, frame_width)
+output_frames_middle = torch.Tensor(batch_size, n_channels, frame_height, frame_width)
+
+-- for training iterations
+for loopIterator=1, trainingCycles, 1
+do
+	print("Training loop:", loopIterator)
+
+	-- get input, output frames (first, last, middle)
+	input_frames[1][1] = trainingFrames[loopIterator] -- first
+	input_frames[1][2] = trainingFrames[loopIterator + 2] -- last
+	output_frames_middle[1] = trainingFrames[loopIterator + 1] -- middle
+
+	-- forward data and print loss
+	loss = criterion:forward(model:forward(input_frames), output_frames_middle)
+	print(loss)
+
+	-- (1) zero the accumulation of the gradients
+	model:zeroGradParameters()
+	-- (2) accumulate gradients
+	model:backward(input_frames, criterion:backward(model.output, output_frames_middle))
+	-- (3) update parameters with a varied learning rate
+	if loss > 0.12 then
+		model:updateParameters(0.20)
+	elseif loss > 0.075 then
+		model:updateParameters(0.07)
+	else
+		model:updateParameters(0.05)
+	end
+end
+
+
+
+-- TESTING
+
+print("Testing...")
+
+-- input_frames = torch.Tensor(batch_size, 2, n_channels, frame_height, frame_width)
+-- output_frames_middle = torch.Tensor(batch_size, n_channels, frame_height, frame_width)
+
+for loopIterator=1, sizeOfTestSet-1, 1
+do
+	-- get input frames, output frame (first, last, middle)
+	input_frames[1][1] = trainingFrames[loopIterator] -- first
+	input_frames[1][2] = trainingFrames[loopIterator + 2] -- last
+	
+	groundTruthFrame = torch.Tensor(batch_size, n_channels, frame_height, frame_width)
+	groundTruthFrame[1] = testingFrames[loopIterator+1]
+
+	loss = criterion:forward(model:forward(input_frames), groundTruthFrame)
+	predictedFrame = model.output
+
+
+	-- print(output_frames_middle) --1x3x96x160
+	-- print(groundTruthFrame) -- 1x3x96x160
+	-- print(predictedFrame) --1x3x96x160
+
+	print(loss)
+	tempPredictedImage = predictedFrame[1]
+	tempGroundTruth = groundTruthFrame[1]
+
+	filename = "Original" .. tostring(loopIterator) .. ".jpg"
+	image.save(filename, tempGroundTruth)
+
+	filename = "Predicted" .. tostring(loopIterator) .. ".jpg"
+	image.save(filename, tempPredictedImage)
+end
+
+
+
+
+-- -- FIXME: Remove SANITY CHECK
+-- predictedImage = torch.Tensor(3,128,128)
+-- predictedImage = input_frames[50][2]
+
+-- filename = "test.jpg"
+-- image.save(filename, predictedImage)
+
 
