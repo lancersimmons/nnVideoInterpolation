baf86770e8907f378496760fc4e894779a074004
BuildTools <lancersimmons@gmail.com>
2016-12-09 01:40:00 -0500

    model save and load impl

--- cnn_gpu.lua
+++ cnn_gpu.lua
@@ -15,7 +15,15 @@ require 'cunn'
 math.randomseed( os.time() )
 
 -- PARAMETERS:
-numberOfInputFramesToUse = 402
+numberOfInputFramesToUse = 3000
+
+modelToLoadFromFile = nil
+if arg[1] ~= nil then
+	loadModelFlag = true
+	modelToLoadFromFile = arg[1]
+else
+	loadModelFlag = false
+end
 
 --3 for triplets, 5 for quintuplets, etc.
 frameGroupSize = 3
@@ -129,6 +137,8 @@ for i=1, sizeOfTrainingSet, 1 do
 	image.hflip(trainingFrames[i+sizeOfTrainingSet*2], trainingFrame[temp])
 end
 ]]--
+
+
 --  build testing set
 testingFrames = 
 	torch.Tensor(
@@ -151,157 +161,129 @@ trainingFrames = trainingFrames - 1
 testingFrames = testingFrames * 2
 testingFrames = testingFrames - 1
 
--- FIXME: Fix the Tanh/relu scaling issue
------------- Model ------------------------------------------------
+if loadModelFlag then
+	print("Loading model from file...")
+	model = torch.load(modelToLoadFromFile)
+else
+	------------ Model ------------------------------------------------
 
-criterion = nn.AbsCriterion()
-criterion = criterion:cuda()
-s1 = nn.Sequential()
-s01 = nn.Sequential()
-s2 = nn.Sequential()
-s3 = nn.Sequential()
-s4 = nn.Sequential()
-s5 = nn.Sequential()
-s6 = nn.Sequential()
-s7 = nn.Sequential()
-s8 = nn.Sequential()
-s9 = nn.Sequential()
-s10 = nn.Sequential()
-model = nn.Sequential()
+	criterion = nn.AbsCriterion()
+	criterion = criterion:cuda()
+	s1 = nn.Sequential()
+	s01 = nn.Sequential()
+	s2 = nn.Sequential()
+	s3 = nn.Sequential()
+	s4 = nn.Sequential()
+	s5 = nn.Sequential()
+	s6 = nn.Sequential()
+	s7 = nn.Sequential()
+	s8 = nn.Sequential()
+	s9 = nn.Sequential()
+	s10 = nn.Sequential()
+	model = nn.Sequential()
 
-c1 = nn.Parallel(1, 1)
-c2 = nn.DepthConcat(1)
-c3 = nn.DepthConcat(1)
-c4 = nn.DepthConcat(1)
-c5 = nn.DepthConcat(1)
+	c1 = nn.Parallel(1, 1)
+	c2 = nn.DepthConcat(1)
+	c3 = nn.DepthConcat(1)
+	c4 = nn.DepthConcat(1)
+	c5 = nn.DepthConcat(1)
 
-s1:add(nn.SpatialConvolution(3, 96, 3, 3, 1, 1, 1, 1):cuda()) ----- frame 1 conv block 1
-s1:add(nn.Tanh():cuda())
-s1:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1):cuda())
-s1:add(nn.Tanh():cuda())
-s1:add(nn.SpatialMaxPooling(2, 2, 2, 2):cuda())		--- ouput_size: 96 x fr_w/2 x fr_h/2
+	s1:add(nn.SpatialConvolution(3, 96, 3, 3, 1, 1, 1, 1):cuda()) ----- frame 1 conv block 1
+	s1:add(nn.Tanh():cuda())
+	s1:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1):cuda())
+	s1:add(nn.Tanh():cuda())
+	s1:add(nn.SpatialMaxPooling(2, 2, 2, 2):cuda())		--- ouput_size: 96 x fr_w/2 x fr_h/2
 
-s2:add(nn.SpatialConvolution(3, 96, 3, 3, 1, 1, 1, 1):cuda()) ----- frame 3 conv block 1
-s2:add(nn.Tanh():cuda())
-s2:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1):cuda())
-s2:add(nn.Tanh():cuda())
-s2:add(nn.SpatialMaxPooling(2, 2, 2, 2):cuda())		--- ouput_size: 96 x fr_w/2 x fr_h/2
-
-
-c1:add(s1)
-c1:add(s2)											--- ouput_size: (96+96) x fr_w/2 x fr_h/2
-
-s01:add(c1)
-s01:add(nn.SpatialConvolution(192, 96, 3, 3, 1, 1, 1, 1):cuda()) ------ frame 1 conv block 2
-s01:add(nn.Tanh():cuda())
-s01:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1):cuda())
-s01:add(nn.Tanh():cuda())	
-s01:add(nn.SpatialMaxPooling(2, 2, 2, 2):cuda())		--- ouput_size: 96 x fr_w/4 x fr_h/4
+	s2:add(nn.SpatialConvolution(3, 96, 3, 3, 1, 1, 1, 1):cuda()) ----- frame 3 conv block 1
+	s2:add(nn.Tanh():cuda())
+	s2:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1):cuda())
+	s2:add(nn.Tanh():cuda())
+	s2:add(nn.SpatialMaxPooling(2, 2, 2, 2):cuda())		--- ouput_size: 96 x fr_w/2 x fr_h/2
 
 
-s3:add(s01)								--------------------- Conv block 3
-s3:add(nn.SpatialConvolution(96, 128, 3, 3, 1, 1, 1, 1):cuda()) 
-s3:add(nn.Tanh():cuda())
-s3:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda())
-s3:add(nn.Tanh():cuda())
-s3:add(nn.SpatialMaxPooling(2, 2, 2, 2):cuda())		--- ouput_size: 128 x fr_w/8 x fr_h/8
+	c1:add(s1)
+	c1:add(s2)											--- ouput_size: (96+96) x fr_w/2 x fr_h/2
+
+	s01:add(c1)
+	s01:add(nn.SpatialConvolution(192, 96, 3, 3, 1, 1, 1, 1):cuda()) ------ frame 1 conv block 2
+	s01:add(nn.Tanh():cuda())
+	s01:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1):cuda())
+	s01:add(nn.Tanh():cuda())	
+	s01:add(nn.SpatialMaxPooling(2, 2, 2, 2):cuda())		--- ouput_size: 96 x fr_w/4 x fr_h/4
 
 
-s4:add(s3)								--------------------- Conv block 4
-s4:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda()) 
-s4:add(nn.Tanh():cuda())
-s4:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda())
-s4:add(nn.Tanh():cuda())
-s4:add(nn.SpatialMaxPooling(2, 2, 2, 2):cuda())		--- ouput_size: 128 x fr_w/16 x fr_h/16
+	s3:add(s01)								--------------------- Conv block 3
+	s3:add(nn.SpatialConvolution(96, 128, 3, 3, 1, 1, 1, 1):cuda()) 
+	s3:add(nn.Tanh():cuda())
+	s3:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda())
+	s3:add(nn.Tanh():cuda())
+	s3:add(nn.SpatialMaxPooling(2, 2, 2, 2):cuda())		--- ouput_size: 128 x fr_w/8 x fr_h/8
 
 
-s5:add(s4)								--------------------- Conv block 5
-s5:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda()) 
-s5:add(nn.Tanh():cuda())
-s5:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda())
-s5:add(nn.Tanh():cuda())
-s5:add(nn.SpatialMaxPooling(2, 2, 2, 2):cuda())		--- ouput_size: 128 x fr_w/32 x fr_h/32
+	s4:add(s3)								--------------------- Conv block 4
+	s4:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda()) 
+	s4:add(nn.Tanh():cuda())
+	s4:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda())
+	s4:add(nn.Tanh():cuda())
+	s4:add(nn.SpatialMaxPooling(2, 2, 2, 2):cuda())		--- ouput_size: 128 x fr_w/16 x fr_h/16
 
 
-s6:add(s5)								--------------------- DeConv block 1
-s6:add(nn.SpatialFullConvolution(128, 128, 4, 4, 2, 2, 1, 1):cuda())
-s6:add(nn.Tanh():cuda())
-s6:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda()) 
-s6:add(nn.Tanh():cuda())									--- ouput_size: 128 x fr_w/16 x fr_h/16
-
-c2:add(s4)
-c2:add(s6)											--- ouput_size: (128+128) x fr_w/16 x fr_h/16
-
-s7:add(c2)								--------------------- DeConv block 2
-s7:add(nn.SpatialFullConvolution(256, 128, 4, 4, 2, 2, 1, 1):cuda())
-s7:add(nn.Tanh():cuda())
-s7:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda()) 
-s7:add(nn.Tanh():cuda())									--- ouput_size: 128 x fr_w/8 x fr_h/8
-
-c3:add(s3)
-c3:add(s7)											--- ouput_size: (128+128) x fr_w/16 x fr_h/16
-
-s8:add(c3)								--------------------- DeConv block 3
-s8:add(nn.SpatialFullConvolution(256, 128, 4, 4, 2, 2, 1, 1):cuda())
-s8:add(nn.Tanh():cuda())
-s8:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda()) 
-s8:add(nn.Tanh():cuda())									--- ouput_size: 128 x fr_w/4 x fr_h/4
-
-c4:add(s01)
-c4:add(s8)											--- ouput_size: (192+128) x fr_w/16 x fr_h/16
-
-s9:add(c4)								--------------------- DeConv block 4
-s9:add(nn.SpatialFullConvolution(224, 96, 4, 4, 2, 2, 1, 1):cuda())
-s9:add(nn.Tanh():cuda())
-s9:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1):cuda()) 
-s9:add(nn.Tanh():cuda())									--- ouput_size: 96 x fr_w/2 x fr_h/2
-
-c5:add(s9)
-c5:add(c1)
-
-s10:add(c5)								--------------------- DeConv block 5
-s10:add(nn.SpatialFullConvolution(288, 96, 4, 4, 2, 2, 1, 1):cuda())
-s10:add(nn.Tanh():cuda())
-s10:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1):cuda()) 
-s10:add(nn.Tanh():cuda())									--- ouput_size: 96 x fr_w/1 x fr_h/1
-
-model:add(s10)							--------------------- Output Layer
-model:add(nn.SpatialConvolution(96, 3, 3, 3, 1, 1, 1, 1):cuda())
-model:add(nn.Tanh():cuda())									--- ouput_size: 3 x fr_w/1 x fr_h/1
-model = model:cuda()
--------------------------------------------------------------------
-
--- rand_inputs = torch.randn(batch_size, 2, n_channels, frame_height, frame_width)
--- rand_inputs1 = torch.randn(n_channels, frame_height, frame_width)
-
--- -- these tensors will hold the outer and inner frames of the triplets, respectively
--- input_frames = torch.Tensor(batch_size, 2, n_channels, frame_height, frame_width)
--- output_frames_middle = torch.Tensor(batch_size, n_channels, frame_height, frame_width)
-
--- -- FIXME: Ensure generalization of this code for more input sizes / parameters
--- -- This will move necessary frames into the input_frames, output_frames_middle tensors
--- for frameNumber=1, batch_size, 1
--- do
--- 	-- print("Frame number in batch: ", frameNumber) -- 1 to 100 inclusive
-	
--- 	input_frames[0+frameNumber][1] = trainingFrames[0+frameNumber]
--- 	input_frames[0+frameNumber][2] = trainingFrames[0+frameNumber+2]
-
--- 	output_frames_middle[0+frameNumber] = trainingFrames[0+frameNumber+1]
-
--- end
+	s5:add(s4)								--------------------- Conv block 5
+	s5:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda()) 
+	s5:add(nn.Tanh():cuda())
+	s5:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda())
+	s5:add(nn.Tanh():cuda())
+	s5:add(nn.SpatialMaxPooling(2, 2, 2, 2):cuda())		--- ouput_size: 128 x fr_w/32 x fr_h/32
 
 
-print("Forwarding...")
--- forward random test inputs
--- aa = model:forward(rand_inputs)
--- forward real inputs
--- aa = model:forward(input_frames)
--- print(aa)
+	s6:add(s5)								--------------------- DeConv block 1
+	s6:add(nn.SpatialFullConvolution(128, 128, 4, 4, 2, 2, 1, 1):cuda())
+	s6:add(nn.Tanh():cuda())
+	s6:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda()) 
+	s6:add(nn.Tanh():cuda())									--- ouput_size: 128 x fr_w/16 x fr_h/16
 
+	c2:add(s4)
+	c2:add(s6)											--- ouput_size: (128+128) x fr_w/16 x fr_h/16
 
+	s7:add(c2)								--------------------- DeConv block 2
+	s7:add(nn.SpatialFullConvolution(256, 128, 4, 4, 2, 2, 1, 1):cuda())
+	s7:add(nn.Tanh():cuda())
+	s7:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda()) 
+	s7:add(nn.Tanh():cuda())									--- ouput_size: 128 x fr_w/8 x fr_h/8
 
--- TRAINING
+	c3:add(s3)
+	c3:add(s7)											--- ouput_size: (128+128) x fr_w/16 x fr_h/16
+
+	s8:add(c3)								--------------------- DeConv block 3
+	s8:add(nn.SpatialFullConvolution(256, 128, 4, 4, 2, 2, 1, 1):cuda())
+	s8:add(nn.Tanh():cuda())
+	s8:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1):cuda()) 
+	s8:add(nn.Tanh():cuda())									--- ouput_size: 128 x fr_w/4 x fr_h/4
+
+	c4:add(s01)
+	c4:add(s8)											--- ouput_size: (192+128) x fr_w/16 x fr_h/16
+
+	s9:add(c4)								--------------------- DeConv block 4
+	s9:add(nn.SpatialFullConvolution(224, 96, 4, 4, 2, 2, 1, 1):cuda())
+	s9:add(nn.Tanh():cuda())
+	s9:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1):cuda()) 
+	s9:add(nn.Tanh():cuda())									--- ouput_size: 96 x fr_w/2 x fr_h/2
+
+	c5:add(s9)
+	c5:add(c1)
+
+	s10:add(c5)								--------------------- DeConv block 5
+	s10:add(nn.SpatialFullConvolution(288, 96, 4, 4, 2, 2, 1, 1):cuda())
+	s10:add(nn.Tanh():cuda())
+	s10:add(nn.SpatialConvolution(96, 96, 3, 3, 1, 1, 1, 1):cuda()) 
+	s10:add(nn.Tanh():cuda())									--- ouput_size: 96 x fr_w/1 x fr_h/1
+
+	model:add(s10)							--------------------- Output Layer
+	model:add(nn.SpatialConvolution(96, 3, 3, 3, 1, 1, 1, 1):cuda())
+	model:add(nn.Tanh():cuda())									--- ouput_size: 3 x fr_w/1 x fr_h/1
+	model = model:cuda()
+	-------------------------------------------------------------------
+end
 
 
 -- these tensors will hold the outer and inner frames of the triplets, respectively
@@ -309,6 +291,14 @@ input_frames = torch.Tensor(2, n_channels, frame_height, frame_width)
 input_frames = input_frames:cuda()
 output_frames_middle = torch.Tensor(n_channels, frame_height, frame_width)
 output_frames_middle = output_frames_middle:cuda()
+
+
+if loadModelFlag then
+	print("Skipping training...")
+else
+-- TRAINING
+print("Forwarding...")
+
 -- for training iterations
 local theta, gradTheta = model:getParameters()
 local optimState = {learningRate = 0.01, momentum = 0, learningRateDecay = 5e-7}model:training()
@@ -370,9 +360,7 @@ end
 
 torch.save("myModel", model)
 
-
-
-
+end
 
 
 
@@ -411,6 +399,7 @@ do
 
 	tempGroundTruth = tempGroundTruth + 1
 	tempGroundTruth = tempGroundTruth / 2
+
 
 	filename = tostring(odd).. ".jpg"
 	image.save(filename, tempGroundTruth)
