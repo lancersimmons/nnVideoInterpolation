a35c6f9c33aabe4a89461204fe94214ee8564acd
Lance Simmons <lancersimmons@gmail.com>
2016-12-12 22:16:16 -0500

    simplified demo to only do video generation

--- demo.sh
+++ demo.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# Demo script to exercise other scripts in order.
+# Demo script to generate videos
 
-echo "Hello, $USER."
-echo "Place the .mp4 file you would like to use in the same directory as the scripts."
-echo "Enter your .mp4 file name and hit [ENTER]:"
-read videoFileName
-echo "$videoFileName"
-echo "Starting training pipeline."
-echo "Call python script"
-python frameExtractor.py $videoFileName
-sleep 1s
-echo "Starting CNN training and testing."
-th cnn_gpu.lua
-sleep 1s
+# echo "Hello, $USER."
+# echo "Place the .mp4 file you would like to use in the same directory as the scripts."
+# echo "Enter your .mp4 file name and hit [ENTER]:"
+# read videoFileName
+# echo "$videoFileName"
+# echo "Starting training pipeline."
+# echo "Call python script"
+# python frameExtractor.py $videoFileName
+# sleep 1s
+# echo "Starting CNN training and testing."
+# th cnn_gpu.lua
+# sleep 1s
 cd prediction
 ffmpeg -f image2 -r 30 -i PredictedSeq%05d.jpg -vcodec mpeg4 -y interpolated.mp4
 mv interpolated.mp4 ..
